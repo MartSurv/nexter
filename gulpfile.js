a5ae1bd861022b45cfdcb683018a235fa4ab8965
Martynas <47948087+MartSurv@users.noreply.github.com>
2020-08-14 14:14:17 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+//Main
+const gulp = require('gulp');
+
+//Styles
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+//Images
+const imagemin = require('gulp-imagemin');
+
+var paths = {
+    styles: {
+        src: 'sass/**/*.scss',
+        dest: 'dist/css/'
+    },
+    images: {
+        src: 'img/*',
+        dest: 'dist/img'
+    }
+};
+
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
+function images() {
+    return gulp.src(paths.images.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.images.dest));
+}
+
+function watch() {
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.images.src, images);
+}
+
+exports.default = watch;
